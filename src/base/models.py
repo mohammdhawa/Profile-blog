c8dda26e2ac1cfd854925322961928b1dcd2e3a8
Muhammed Recep Hava <ragab5434@gmail.com>
2023-06-29 17:21:41 +0300

    added CRUD functionality

--- src/base/models.py
+++ src/base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    headline = models.CharField(max_length=200)
+    sub_headline = models.CharField(max_length=200, null=True, blank=True)
+    thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="placeholder.png")
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    tag = models.ManyToManyField(Tag)
+    # slug = 
+
+    def __str__(self):
+        return self.headline
--- src/base/views.py
+++ src/base/views.py
@@ -1,37 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required   
 # Create your views here.
 
+from .models import Tag, Post
+from .forms import PostForm
+
 def home(request):
-    
-    context = {}
+    posts = Post.objects.filter(active=True, featured=True)[:3]
+
+    context = {'posts': posts}
     return render(request, 'base/index.html', context)
 
 def posts(request):
-    
-    posts = [
-        {
-            'headline': 'Laboratory Management System',
-            'sub_headline': 'Designed build & matained a the lab management system for FOI laboratory',
-        },
-        {
-            'headline': 'Online Store - Course',
-            'sub_headline': 'Online store with paypal payment integration and guest user shopping',
-        },
-        {
-            'headline': 'Membership Website',
-            'sub_headline': "Modulized guide for online courses with step by step instructions",
-        }
-    ]
+    posts = Post.objects.filter(active=True)
 
     context = {'posts': posts}
     return render(request, 'base/posts.html', context)
 
-def post(request):
-    
-    context = {}
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+
+    context = {'post': post}
     return render(request, 'base/post.html', context)
 
 def profile(request):
     
     context = {}
     return render(request, 'base/profile.html', context)
+
+
+# CRUD views
+
+@login_required(login_url="base:home")
+def createPost(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('base:posts')
+
+    context = {'form': form}
+    return render(request, 'base/post_form.html', context)
+
+
+@login_required(login_url="base:home")
+def updatetPost(request, pk):
+    post = Post.objects.get(id=pk)
+
+    form = PostForm(instance=post)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+        return redirect('base:posts')
+
+    context = {'form': form}
+    return render(request, 'base/post_form.html', context)
+
+
+@login_required(login_url="base:home")
+def deletePost(request, pk):
+    post = Post.objects.get(id=pk)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect("base:posts")
+
+    context = {'item': post}
+    return render(request, 'base/delete.html', context)
